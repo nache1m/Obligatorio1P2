133b08749e370e0a4027bce91353a5fca7c4aa0a
elnac <elnac@Nacho>
2023-10-01 15:31:30 -0300

    Se agrego menu de opciones a clase Menu.

--- src/Interfaz/Menu.java
+++ src/Interfaz/Menu.java
@@ -1,6 +1,8 @@
 
 package Interfaz;
 
+import java.util.*;
+
 public class Menu {
     public static void imprimirTablero(int mat[][]){
         // Indices de columnas
@@ -27,6 +29,50 @@ public class Menu {
                 System.out.print("---+");
             }
             System.out.println("");
+        }
+    }
+    
+    public static void imprimirMenu(){
+        Scanner lector = new Scanner(System.in);
+        boolean salir = true;
+        int opcion;
+        System.out.println("¿Se desea comenzar una nueva partida? (S/N)");
+        String deseaJugar = lector.nextLine();
+        if (deseaJugar == "N"){
+            System.out.println("¡Nos vemos pronto!");
+        } else {
+            salir = false;
+            while(!salir){
+                System.out.println("1. Tomar datos desde un archivo.");
+                System.out.println("2. Usar tablero predefinido.");
+                System.out.println("3. Usar tablero al azar.");
+                System.out.println("4. Salir del menú.");
+            try {
+ 
+                System.out.println("Escribe una de las opciones");
+                opcion = lector.nextInt();
+ 
+                switch (opcion) {
+                    case 1:
+                        // Llamada del metodo.
+                        break;
+                    case 2:
+                        // Llamada del metodo.
+                        break;
+                    case 3:
+                        // Llamada del metodo.
+                        break;
+                    case 4:
+                        salir = true;
+                        break;
+                    default:
+                        System.out.println("Debe ingresar un número entre 1 y 4.");
+                }
+            } catch (InputMismatchException e) {
+                System.out.println("Debes insertar un número");
+                lector.next();
+                }
+            }
         }
     }
 }
